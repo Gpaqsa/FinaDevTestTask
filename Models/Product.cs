d9afa4df16c46c10d1146de1cc8471901d16da22
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2024-03-03 16:34:50 +0400

    add Model and Controller

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FinaDevTestTask.Models
+{
+    public class Product
+    {
+        // Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
+
---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace FinaDevTestTask
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        }
+    }
+}
+
